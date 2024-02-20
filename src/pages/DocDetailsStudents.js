7ec4200ed7dca1da8ec731566d07b5bdfaf54c94
songwut naksawat <6451000886@rumail.ru.ac.th>
2024-02-20 21:53:22 +0700

    update

--- src/pages/DocDetailsStudents.js
+++ src/pages/DocDetailsStudents.js
@@ -1,20 +1,27 @@
 import React from "react";
 import HeaderStudent from "../components/HeaderStudent";
-import Button from "react-bootstrap/Button";
-import { MDBTable, MDBTableHead, MDBTableBody } from "mdb-react-ui-kit";
 import "./DocDetailsStudents.css";
+import { useNavigate } from "react-router-dom";
 
 const DocDetailsStudents = () => {
+    const navigate = useNavigate();
+    const onDocx1 = () => {
+        navigate("/Docx1");
+    };
     return (
         <div>
             <HeaderStudent />
             <br></br>
             <br></br>
             <hr></hr>
-
-            <button class="button button2">Shadow on Hover</button>
-            <button class="button button2">Shadow on Hover</button>
-            <button class="button button2">Shadow on Hover</button>
+            <div className="DocDetails">
+                {" "}
+                <button class="button1 button2" onClick={onDocx1}>
+                    กรอกเอกสารป.1
+                </button>
+                <button class="button1 button2">เเก้ไขเอกสารป.1</button>
+                <button class="button1 button2">Download</button>
+            </div>
         </div>
     );
 };
